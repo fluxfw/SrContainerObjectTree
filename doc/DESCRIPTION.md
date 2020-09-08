7fb25a8db06ce1956d9c53d568be7e5f678857cf
Florian Wyss <fw@studer-raimann.ch>
2020-09-08 12:24:48 +0200

    empty

--- doc/DESCRIPTION.md
+++
@@ -1,1 +0,0 @@
-# SrCurriculum ILIAS Plugin Description
--- sql/dbupdate.php
+++
@@ -1,8 +0,0 @@
-<#1>
-<?php
-\srag\Plugins\SrCurriculum\Repository::getInstance()->installTables();
-?>
-<#2>
-<?php
-\srag\Plugins\SrCurriculum\Repository::getInstance()->installTables();
-?>
